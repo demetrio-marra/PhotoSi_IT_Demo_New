aca5d761c012cd08a2eb44b5405c5404ab248c20
dmarra <dmarra@20221050-AIT>
2022-10-05 18:40:52 +0200

    bugfix

--- PhotoSi_IT_Demo_New.Services.Ordini/Program.cs
+++ PhotoSi_IT_Demo_New.Services.Ordini/Program.cs
@@ -11,6 +11,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<OrdiniDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
--- PhotoSi_IT_Demo_New.Services.Recapiti/Program.cs
+++ PhotoSi_IT_Demo_New.Services.Recapiti/Program.cs
@@ -9,6 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<RecapitiDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
